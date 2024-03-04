e9458452c8d94061a3cd41fd19152833173945c8
HowletH1 <danila23482@gmail.com>
2024-03-04 18:16:00 +0300

    main

---
+++ main.py
@@ -0,0 +1,19 @@
+import json
+
+
+from parser_folder.parser import OframParser
+
+
+def main():
+    parser = OframParser()
+    parser.parse_card()
+    json_string = json.dumps(parser.card, ensure_ascii=False)
+    with open("product_card.json", "w", encoding="utf-8") as f:
+        f.write(json_string)
+    print(json_string)
+    print("Картинки сохранены в .../ofram-parser/parser_folder/название товара")
+
+
+if __name__ == "__main__":
+    main()
+
