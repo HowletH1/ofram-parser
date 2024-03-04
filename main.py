15cb8cffb1ab8e3d803fc96eff39cc3abc605ba9
HowletH1 <danila23482@gmail.com>
2024-03-04 18:23:22 +0300

    main

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main():
     with open("product_card.json", "w", encoding="utf-8") as f:
         f.write(json_string)
     print(json_string)
-    print("Картинки сохранены в .../ofram-parser/parser_folder/название товара")
+    print("Картинки сохранены в .../ofram-parser/название товара")
 
 
 if __name__ == "__main__":
